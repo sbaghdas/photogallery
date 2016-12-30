6b9d0956fc7932c9122c2b61af776de8192f6357
Suren Baghdasaryan <Suren@Surens-MacBook-Pro.local>
2016-12-30 10:43:42 -0800

    Display fetched titles in the RecyclerView

--- app/src/main/java/com/example/suren/photogallery/FetchItemsTask.java
+++ app/src/main/java/com/example/suren/photogallery/FetchItemsTask.java
@@ -4,15 +4,33 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.IOException;
+import java.util.List;
 
-public class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+public class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
     private static final String LOG_TAG = FetchItemsTask.class.getSimpleName();
     private static final String SITE_URL = "https://www.bignerdranch.com";
 
+    public interface Listener {
+        void onListFetched(List<GalleryItem> list);
+    }
+
+    private Listener mListener;
+
+    public FetchItemsTask(Listener listener) {
+        super();
+        mListener = listener;
+    }
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        FlickrFetchr.fetchItems();
-        return null;
+    protected List<GalleryItem> doInBackground(Void... voids) {
+        return FlickrFetchr.fetchItems();
     }
+
+    @Override
+    protected void onPostExecute(List<GalleryItem> galleryItems) {
+        if (mListener != null) {
+            mListener.onListFetched(galleryItems);
+        }
+    }
+
 }
--- app/src/main/java/com/example/suren/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/suren/photogallery/PhotoGalleryFragment.java
@@ -9,10 +9,51 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class PhotoGalleryFragment extends Fragment {
+public class PhotoGalleryFragment extends Fragment implements FetchItemsTask.Listener {
     private RecyclerView mRecyclerView;
+    private List<GalleryItem> mItems = new ArrayList<GalleryItem>();
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView mCaptionTextView;
+
+        public PhotoHolder(View itemView) {
+            super(itemView);
+            mCaptionTextView = (TextView)itemView;
+        }
+
+        public void bindGalleryItem(GalleryItem item) {
+            mCaptionTextView.setText(item.getCaption());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> mItems;
+
+        public PhotoAdapter(List<GalleryItem> items) {
+            mItems = items;
+        }
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new PhotoHolder(new TextView(getActivity()));
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+            holder.bindGalleryItem(mItems.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+    }
 
     public PhotoGalleryFragment() {
     }
@@ -28,8 +69,23 @@ public class PhotoGalleryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         mRecyclerView = (RecyclerView)view.findViewById(R.id.fragment_photo_gallery_recycler_view);
         mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
         setRetainInstance(true);
-        new FetchItemsTask().execute();
+        new FetchItemsTask(this).execute();
         return view;
+    }
+
+    @Override
+    public void onListFetched(List<GalleryItem> list) {
+        mItems = list;
+        setupAdapter();
+    }
+
+    private void setupAdapter() {
+        // this check is necessary because PhotoAdapter uses getActivity() which might be null
+        // if fragment was not attached to activity yet
+        if (isAdded()) {
+            mRecyclerView.setAdapter(new PhotoAdapter(mItems));
+        }
     }
 }
