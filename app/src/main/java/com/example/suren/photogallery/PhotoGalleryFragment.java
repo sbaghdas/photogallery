2d721b8be8f0470b4048d0dafe2f9c466be5c786
Suren Baghdasaryan <Suren@Surens-MacBook-Pro.local>
2016-12-30 09:23:34 -0800

    Add RecyclerView to main fragment

--- app/src/main/java/com/example/suren/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/example/suren/photogallery/PhotoGalleryFragment.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class PhotoGalleryFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
     public PhotoGalleryFragment() {
     }
 
@@ -21,6 +25,9 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        return view;
     }
 }
